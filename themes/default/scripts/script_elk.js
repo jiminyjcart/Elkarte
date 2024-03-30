51f9873e72f769cb7b1c1ee91f16caee70866f5a
Spuds <spuds@spudsdesign.com>
2024-03-30 11:01:39 -0500

    ! pull tooltips to its own file

--- themes/default/scripts/script_elk.js
+++ themes/default/scripts/script_elk.js
@@ -611,156 +611,6 @@ function revalidateMentions (sForm, sInput)
 })(jQuery);
 
 /**
- * SiteTooltip, Basic JavaScript function to provide styled tooltips
- *
- * - shows the tooltip in a div with the class defined in tooltipClass
- * - moves all selector titles to a hidden div and removes the title attribute to
- *   prevent any default browser actions
- * - attempts to keep the tooltip on screen
- *
- */
-class SiteTooltip
-{
-	constructor (settings = {})
-	{
-		this.defaults = {
-			tooltipID: 'site_tooltip', // ID used on the outer div
-			tooltipTextID: 'site_tooltipText', // ID on the inner div holding the text
-			tooltipClass: 'tooltip', // The class applied to the sibling span, defaults provides a fade cover
-			tooltipSwapClass: 'site_swaptip', // a class only used internally, change only if you have a conflict
-			tooltipContent: 'html' // display captured title text as html or text
-		};
-
-		// Account for any user options
-		this.settings = Object.assign({}, this.defaults, settings);
-	}
-
-	/**
-	 * Creates tooltips for elements.
-	 *
-	 * @param {string} elem - The CSS selector of the elements to create tooltips for.
-	 *
-	 * @returns {null} - Returns null if the device is mobile or touch-enabled.
-	 */
-	create (elem)
-	{
-		// No need here
-		if (is_mobile || is_touch)
-		{
-			return null;
-		}
-
-		// Move passed selector titles to a hidden span, then remove the selector title to prevent any default browser actions
-		for (let el of document.querySelectorAll(elem))
-		{
-			let title = el.getAttribute('title');
-
-			el.setAttribute('data-title', title);
-			el.removeAttribute('title');
-			el.addEventListener('mouseenter', this.showTooltip.bind(this));
-			el.addEventListener('mouseleave', this.hideTooltip.bind(this));
-		}
-	}
-
-	/**
-	 * Positions the tooltip element relative to the provided event target.
-	 *
-	 * @param {Event} event - The event object that triggered the tooltip placement.
-	 */
-	positionTooltip (event)
-	{
-		let tooltip = document.getElementById(this.settings.tooltipID);
-		if (!tooltip)
-		{
-			return;
-		}
-
-		let rect = event.target.getBoundingClientRect();
-		let tooltipHeight = tooltip.offsetHeight;
-		let viewportHeight = window.innerHeight;
-
-		let x = rect.left;
-		// Initially trying to position above the element
-		let y = window.scrollY + rect.top - tooltipHeight - 5;
-
-		// Don't position above if it falls off-screen, instead move it below
-		if (rect.top - (tooltipHeight + 5) < 0)
-		{
-			y = window.scrollY + rect.bottom + 5;
-		}
-
-		tooltip.style.cssText = 'left:' + x + 'px; top:' + y + 'px';
-	}
-
-	/**
-	 * Displays a tooltip on hover over an element.
-	 *
-	 * @param {Event} event - The event object.
-	 */
-	showTooltip (event)
-	{
-		if (this.tooltipTimeout)
-		{
-			clearTimeout(this.tooltipTimeout);
-		}
-
-		// Represents the timeout for showing a tooltip.
-		this.tooltipTimeout = setTimeout(function() {
-			let title = event.target.getAttribute('data-title');
-			if (title)
-			{
-				// <div id="site_tooltip"><div id="site_tooltipText"><span class="tooltip"
-				let tooltip = document.createElement('div');
-				tooltip.id = this.settings.tooltipID;
-
-				let tooltipText = document.createElement('div');
-				tooltipText.id = this.settings.tooltipTextID;
-
-				let span = document.createElement('span');
-				span.className = this.settings.tooltipClass;
-
-				// Create our element and append it to the body.
-				tooltip.appendChild(tooltipText);
-				tooltip.appendChild(span);
-				document.getElementsByTagName('body')[0].appendChild(tooltip);
-
-				// Load the tooltip content with our data-title
-				if (this.settings.tooltipContent === 'html')
-				{
-					tooltipText.innerHTML = title;
-				}
-				else
-				{
-					tooltipText.innerText = title;
-				}
-
-				tooltip.style.display = 'block';
-				this.positionTooltip(event);
-			}
-		}.bind(this), 1250);
-	}
-
-	/**
-	 * Hides the tooltip.
-	 *
-	 * @param {Event} event - The event object.
-	 */
-	hideTooltip (event)
-	{
-		if (this.tooltipTimeout)
-		{
-			clearTimeout(this.tooltipTimeout);
-		}
-
-		let tooltip = document.getElementById(this.settings.tooltipID);
-		if (tooltip)
-		{
-			tooltip.parentElement.removeChild(tooltip);
-		}
-	}
-}
-
-/**
  * Error box handler class
  *
  * @param {type} oOptions
@@ -926,6 +776,12 @@ function disableAutoComplete ()
 
 /**
  * A system to collect notifications from a single AJAX call and redistribute them among notifiers
+ * via their .send methods
+ *
+ * Current notifiers are:
+ * - ElkDesktop for desktop push notifications
+ * - ElkFavicon for favicon counter
+ *
  */
 (function() {
 	/**
@@ -1019,55 +875,6 @@ function disableAutoComplete ()
 })();
 
 const ElkNotifier = new window.ElkNotifications({});
-
-/**
- * @package   ElkArte Forum
- * @copyright ElkArte Forum contributors
- * @license   BSD http://opensource.org/licenses/BSD-3-Clause (see accompanying LICENSE.txt file)
- *
- * @version 2.0 dev
- *
- * This bits acts as middle-man between the Favico and the ElkNotifications
- * providing the interface required by the latter.
- */
-(function() {
-	const ElkFavicon = (function(opt) {
-
-		opt = (opt) ? opt : {};
-
-		// Holds the favico class
-		let mentions= new Favico(opt);
-
-		// Set the favicon count indicator on startup
-		const init = function(opt) {
-			if (opt.number > 0)
-			{
-				mentions.badge(opt.number);
-				document.querySelector('#button_mentions .pm_indicator').innerHTML = opt.number;
-			}
-		};
-
-		// Update the indicator when new items are added or removed.
-		const send = function(request) {
-			if (request.mentions > 0)
-			{
-				mentions.badge(request.mentions);
-				document.querySelector('#button_mentions .pm_indicator').innerHTML = request.mentions;
-			}
-			else
-			{
-				mentions.reset();
-			}
-		};
-
-		init(opt);
-		return {
-			send: send
-		};
-	});
-
-	this.ElkFavicon = ElkFavicon;
-})();
 
 /**
  * Initialize the ajax info-bar
@@ -1231,7 +1038,7 @@ async function isFunctionLoaded (selector, limit)
 {
 	let MAX_RETRIES = limit || 180;
 	let retries = 0;
-	while (typeof window[selector] !== 'function' && retries < MAX_RETRIES)
+	while ((typeof window[selector] !== 'function') && retries < MAX_RETRIES)
 	{
 		await new Promise(resolve => requestAnimationFrame(resolve));
 		retries++;
@@ -1259,7 +1066,7 @@ async function isFunctionLoaded (selector, limit)
  * @param {boolean} immediate - Determines whether the function should be executed immediately on the leading edge.
  * @return {Function} - The debounced function.
  */
-function debounce (func, wait, immediate)
+function debounce (func, wait, immediate = false)
 {
 	var timeout;
 
@@ -1320,10 +1127,12 @@ function serialize (form)
 /**
  * Toggles the visibility and height of the specified HTMLElement using animation.
  *
- * Mimics jQ slideToggle, slideUp, slideDown, fadeIn, fadeOut
+ * Mimics jQ slideToggle, slideUp, slideDown
  *
  * https://github.com/ericbutler555/plain-js-slidetoggle
  * MIT License
+ *
+ * fadeIn, fadeOut _s2 functions (c) ElkArte BSD
  */
 HTMLElement.prototype.slideToggle = function(duration, callback) {
 	if (this.clientHeight === 0)
